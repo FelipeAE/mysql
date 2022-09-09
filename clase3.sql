d51c482e5ace32c70fbc41aee7751011f33f4725
Felipe Alvarez <32912012+FelipeAE@users.noreply.github.com>
2022-09-09 17:54:43 -0300

    Update clase3.sql

--- clase3.sql
+++ clase3.sql
@@ -29,3 +29,16 @@ insert into autores (nomautor,apeautor) values ("Leonardo"," da Vinci"), ("Vince
 insert into obras (nomobra,idautor) values ("La Gioconda",1), ("La ultima cena",1), ("La noche estrellada",2), ("Guernica",3), ("Las meninas",4), ("Mujer con sombrilla",5);
 insert into ciudades (nomciudad,idpais) values ("Madrid",1), ("Barcelona",1), ("Roma",2), ("Venecia",2), ("Guadalajara",3), ("Monterrey",3), ("Nuevo York",4), ("Temuco",5);
 insert into museos (nom_museo, direccion) values ("Louvre","Paris"), ("Ermitage","San Petersburgo"), ("Tate Modern","Londres"), ("Museos Vaticanos","Roma"), ("Museo Van Gogh","Amsterdam");
+select * from estilos;
+select * from autores;
+select * from museos;
+select * from nacionalidad;
+select * from ciudades;
+select * from paises;
+select * from obras;
+insert into nacionalidad (nom_nacionalidad) values ("Francesa"),("Neerlandes");
+update autores set idnacionalidad = 2 where idautor = 1;
+update autores set idnacionalidad = 6 where idautor = 2;
+update autores set idnacionalidad = 1 where idautor = 3;
+update autores set idnacionalidad = 1 where idautor = 4;
+update autores set idnacionalidad = 5 where idautor = 5;
