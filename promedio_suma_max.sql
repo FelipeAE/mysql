238e76eb41edf10f7e0c48874baca68c0fffeb75
Felipe Alvarez <32912012+FelipeAE@users.noreply.github.com>
2022-09-30 16:22:50 -0300

    Update promedio_suma_max.sql

--- promedio_suma_max.sql
+++ promedio_suma_max.sql
@@ -1,3 +1,4 @@
 select avg(sueldo) as promedio from empleados;
 select sum(sueldo) as suma from empleados;
 select max(sueldo) as maximo from empleados;
+select avg(sueldo),sum(sueldo),max(sueldo) from empleados;
